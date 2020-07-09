67fd00bba9acd5726d9256d66c70cbfcd0e3f205
Joseph cohen <jojocohen57@gmail.com>
2020-07-09 21:17:30 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,14 +1,15 @@
+def fourth_challenge
 epic_tragedy = {
    :montague => {
       :patriarch => {:name => "Lord Montague", :age =>"53"},
       :matriarch => {:name =>"Lady Montague", :age =>"54"},
-      :hero => {:name => "Romeo", :age =>"15", :status =>"alive"},
+      :hero => {:name => "Romeo", :age =>"15"},
       :hero_friends => [
          {:name =>"Benvolio", :age =>"17", :attitude =>"worried"},
-         {:name =>"Mercutio", :age =>"18", :attitude =>"hot-headed"}
+         {:name =>"Mercutio", :age =>"18", :attitude =>"hot-headed"},
       ]
-   }
-    :capulet {
+   },
+    :capulet => {
       :patriarch => {:name =>"Lord Capulet", :age =>"50"},
       :matriarch => {:name =>"Lady Capulet", :age =>"51"},
       :heroine => {:name =>"Juliet", :age =>"15", :status =>"alive"},
@@ -18,3 +19,4 @@ epic_tragedy = {
       ]
    }
 }
+end
--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,8 +1,9 @@
+def third_challenge
 epic_tragedy = {
    :montague => {
-      :patriarch => {:name =>"Lord Montague", :age =>":53"},
-      :matriarch => {:name =>"Lady Montague", :age =>":54"},
-      :hero => {:name =>"Romeo", :age =>"15", :status =>"ali:ve"},
+      :patriarch => {:name =>"Lord Montague", :age =>"53"},
+      :matriarch => {:name =>"Lady Montague", :age =>"54"},
+      :hero => {:name =>"Romeo", :age =>"15", :status =>"alive"},
       :hero_friends => []
    },
    :capulet => {
@@ -12,4 +13,4 @@ epic_tragedy = {
       :heroine_friends => []
    }
 }
- 
+end 
